6dad9bd4c9a97f506b3a5436112fb2b113f6e939
Júlia Tessler <juliatessler@gmail.com>
2023-06-14 17:19:59 -0300

    adds final version

--- generate_dataset.py
+++ generate_dataset.py
@@ -10,19 +10,18 @@ from tqdm.auto import tqdm
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 searcher = LuceneSearcher("indexes/portuguese-lucene-index-msmarco")
 model_name = "unicamp-dl/mMiniLM-L6-v2-en-pt-msmarco-v2"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name).to_device(device)
-query_file_name = "queries_100.tsv"  # for the small version, use queries_100.tsv
+model = AutoModelForSequenceClassification.from_pretrained(model_name).to(device)
+query_file_name = "datasets/portuguese_queries.train.tsv"  # for the small version, use queries_100.tsv
 
 
 def search_with_bm25(query, k=1000):
     return searcher.search(query, k)
 
 
-def reranking(docs, max_len=512, max=1000, batch_size=20):
+def reranking(docs, max_len=512, max=1000, batch_size=500):
     results = []
 
     for i in range(0, len(docs), batch_size):
@@ -34,16 +33,18 @@ def reranking(docs, max_len=512, max=1000, batch_size=20):
         queries_ = [sample["query"] for sample in batch]
         passages_ = [sample["text"] for sample in batch]
 
-        features = tokenizer(
-            queries_,
-            passages_,
-            padding="longest",
-            truncation=True,
-            return_tensors="pt",
-            max_length=max_len,
-        ).to(model.device)
+        with torch.inference_mode():
+            with torch.cuda.amp.autocast():
+                features = tokenizer(
+                    queries_,
+                    passages_,
+                    padding="longest",
+                    truncation=True,
+                    return_tensors="pt",
+                    max_length=max_len,
+                ).to(model.device)
 
-        predictions = model(**features).logits.flatten()
+                predictions = model(**features).logits.flatten()
 
         for score, result in zip(predictions, batch):
             results.append((result, score.item()))
@@ -55,50 +56,55 @@ def reranking(docs, max_len=512, max=1000, batch_size=20):
 
 def generate_list_with_scores():
     print("***** GENERATE LIST WITH SCORES *****")
-    if not os.path.isfile("datasets/dataset_list.pickle"):
+    if not os.path.isfile(f"datasets/dataset_list.pickle"):
         dataset_list = []
         col_names = ["id", "text"]
-        df_queries = pd.read_csv(
-            f"/data/{query_file_name}", names=col_names, delimiter="\t"
-        )
+        df_queries = pd.read_csv(f"{query_file_name}", names=col_names, delimiter="\t")
+
         for index, row in tqdm(df_queries.iterrows(), total=df_queries.shape[0]):
-            # First stage (BM25)
-            hits = search_with_bm25(row["text"], 200)
-            docs = []
+            try:
+                # Fisrt stage (BM25)
+                hits = search_with_bm25(row["text"], 1000)
+                docs = []
 
-            for hit in hits:
-                hit_dict = json.loads(hit.raw)
-                doc = {
-                    "passage_id": int(hit.docid),
-                    "query_id": row["id"],
-                    "query": row["text"],
-                    "text": hit_dict["contents"],
-                }
-                docs.append(doc)
+                for hit in hits:
+                    hit_dict = json.loads(hit.raw)
+                    doc = {
+                        "passage_id": int(hit.docid),
+                        "query_id": row["id"],
+                        "query": row["text"],
+                        "text": hit_dict["contents"],
+                    }
+                    docs.append(doc)
 
-            # Second stage (reranking)
-            docs_reranking = reranking(docs, max=200)
+                # Second stage (reranking)
+                docs_reranking = reranking(docs, max=1000)
 
-            item = {
-                "query_id": docs_reranking[0][0]["query_id"],
-                "positive": {
-                    "id": docs_reranking[0][0]["passage_id"],
-                    "score": docs_reranking[0][1],
-                },
-                "negatives": [],
-            }
+                if len(docs_reranking) > 0:
+                    item = {
+                        "query_id": docs_reranking[0][0]["query_id"],
+                        "positive": {
+                            "id": docs_reranking[0][0]["passage_id"],
+                            "score": docs_reranking[0][1],
+                        },
+                        "negatives": [],
+                    }
 
-            for doc in docs_reranking[-100:]:
-                negative = {"id": doc[0]["passage_id"], "score": doc[1]}
+                    for doc in docs_reranking[-100:]:
+                        negative = {"id": doc[0]["passage_id"], "score": doc[1]}
 
-                item["negatives"].append(negative)
+                        item["negatives"].append(negative)
 
-            dataset_list.append(item)
+                    dataset_list.append(item)
+            except Exception as ex:
+                print(ex)
+                with open(f"datasets/dataset_list.pickle", "wb") as f:
+                    pickle.dump(dataset_list, f)
 
-        with open("datasets/dataset_list.pickle", "wb") as f:
+        with open(f"datasets/dataset_list.pickle", "wb") as f:
             pickle.dump(dataset_list, f)
     else:
-        with open("datasets/dataset_list.pickle", "rb") as input_file:
+        with open(f"datasets/dataset_list.pickle", "rb") as input_file:
             dataset_list = pickle.load(input_file)
 
     return dataset_list
@@ -106,16 +112,15 @@ def generate_list_with_scores():
 
 def generate_dataset():
     dataset_list = generate_list_with_scores()
-
     print("***** GENERATE DATASET TRIPLE *****")
 
     with open(
-        "data/triples.train.small.tsv", "w", encoding="utf8", newline=""
+        f"datasets/triples.train.tsv", "w", encoding="utf8", newline=""
     ) as tsv_file:
         tsv_writer = csv.writer(tsv_file, delimiter="\t", lineterminator="\n")
         for item in tqdm(dataset_list):
             for negative in item["negatives"]:
-                line = f"[{str(item['query_id'])}, [{str(item['positive']['id'])}, {item['positive']['score']}], [{str(negative['id'])}, {negative['score']}]"
+                line = f"[{str(item['query_id'])}, [{str(item['positive']['id'])}, {item['positive']['score']}], [{str(negative['id'])}, {negative['score']}]]"
                 tsv_writer.writerow([line])
 
     print("***** FINISH *****")
