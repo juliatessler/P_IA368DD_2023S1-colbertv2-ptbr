f9a9824d8b268132b553a0ddfc2260fda9573e99
Júlia Tessler <juliatessler@gmail.com>
2023-06-28 19:40:28 -0300

    generalizes code

--- indexing.py
+++ indexing.py
@@ -6,7 +6,7 @@ if __name__=='__main__':
 
         config = ColBERTConfig(
             nbits=2,
-            root="/home/manoel/Documents/Doutorado/P_IA368DD_2023S1/Projeto Final/data/indexes",
+            root="./data/indexes",
         )
-        indexer = Indexer(checkpoint="/home/manoel/Documents/Doutorado/P_IA368DD_2023S1/Projeto Final/ColBERT/experiments/msmarco/none/2023-06/07/15.01.44/checkpoints/colbert-70000", config=config)
-        indexer.index(name="mmarco.nbits=2", collection="/home/manoel/Documents/Doutorado/P_IA368DD_2023S1/Projeto Final/data/collection.tsv", overwrite="resume")
+        indexer = Indexer(checkpoint="/path/to/colbert/checkpoint", config=config)
+        indexer.index(name="mmarco.nbits=2", collection="./data/collection.tsv", overwrite="resume")
--- retrieval.py
+++ retrieval.py
@@ -6,9 +6,9 @@ if __name__=='__main__':
     with Run().context(RunConfig(nranks=1, experiment="mmarco")):
 
         config = ColBERTConfig(
-            root="/home/manoel/Documents/Doutorado/P_IA368DD_2023S1/Projeto Final/ColBERT/experiments/mmarco/indexes/",
+            root="./ColBERT/experiments/mmarco/indexes/",
         )
         searcher = Searcher(index="msmarco.nbits=2", config=config)
-        queries = Queries("/home/manoel/Documents/Doutorado/P_IA368DD_2023S1/Projeto Final/data/portuguese_queries.dev.small.tsv")
+        queries = Queries("./data/portuguese_queries.dev.small.tsv")
         ranking = searcher.search_all(queries, k=100)
         ranking.save("msmarco.nbits=2.ranking.tsv")
